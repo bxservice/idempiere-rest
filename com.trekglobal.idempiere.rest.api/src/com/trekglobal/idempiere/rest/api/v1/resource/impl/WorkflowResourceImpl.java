a78de0cf8eefa09ecf0642a92a0d4b0c7900b9e8
Diego Ruiz <ruizg.diego@gmail.com>
2022-06-13 13:33:32 +0200

    issues11 - Implement getNodes endpoint

--- com.trekglobal.idempiere.rest.api/src/com/trekglobal/idempiere/rest/api/v1/resource/impl/WorkflowResourceImpl.java
+++ com.trekglobal.idempiere.rest.api/src/com/trekglobal/idempiere/rest/api/v1/resource/impl/WorkflowResourceImpl.java
@@ -25,24 +25,72 @@
 **********************************************************************/
 package com.trekglobal.idempiere.rest.api.v1.resource.impl;
 
+import java.util.List;
+
 import javax.ws.rs.core.Response;
 
+import org.compiere.model.I_AD_WF_Activity;
+import org.compiere.model.Query;
+import org.compiere.util.Env;
+import org.compiere.util.Util;
+import org.compiere.wf.MWFActivity;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.trekglobal.idempiere.rest.api.v1.resource.WorkflowResource;
 
 public class WorkflowResourceImpl implements WorkflowResource {
 
 	@Override
 	public Response getNodes() {
-		System.out.println("return all nodes from current user");
-		return null;
+		return getNodes(null);
 	}
 
 	@Override
-	public Response getNodes(String id) {
-		System.out.println("return all nodes from user id = " + id);
-		return null;
-	}
+	public Response getNodes(String userid) {
 
+		List<MWFActivity> activities = getUserUnprocessedSuspendedActivities(userid);
+
+		JsonArray array = new JsonArray();
+		for (MWFActivity activity : activities) {
+			array.add(getActivityJsonObject(activity));
+		}
+		JsonObject json = new JsonObject();
+		json.addProperty("row-count", array.size());
+		json.add("nodes", array);
+		return Response.ok(json.toString()).build();
+	}
+	
+	private List<MWFActivity> getUserUnprocessedSuspendedActivities(String userid) {
+		final String whereClause = "Processed=? AND WFState=? AND AD_User_ID=?";
+		
+		return new Query(Env.getCtx(), I_AD_WF_Activity.Table_Name, whereClause, null)
+				.setClient_ID()
+				.setParameters(false, MWFActivity.WFSTATE_Suspended, getAD_User_ID(userid))
+				.setOnlyActiveRecords(true)
+				.list();
+	}
+	
+	//TODO handle when the UUID is sent instead of the ID
+	private int getAD_User_ID(String userId) {
+		return userId != null ? Integer.parseInt(userId) : Env.getAD_User_ID(Env.getCtx()); 
+	}
+	
+	private JsonObject getActivityJsonObject(MWFActivity activity) {
+		JsonObject json = new JsonObject();
+		json.addProperty("id", activity.getAD_WF_Activity_ID());
+		if (!Util.isEmpty(activity.getAD_WF_Activity_UU())) {
+			json.addProperty("uid", activity.getAD_WF_Activity_UU());
+		}
+		json.addProperty("model-name", activity.get_TableName().toLowerCase());
+		json.addProperty("node-name", activity.getNodeName());
+		if (!Util.isEmpty(activity.getNodeDescription())) {
+			json.addProperty("node-description", activity.getNodeDescription());
+		}
+		
+		return json;
+	}
+	
 	@Override
 	public Response approve(String nodeId, String jsonText) {
 		System.out.println("approve node: "+ nodeId);
