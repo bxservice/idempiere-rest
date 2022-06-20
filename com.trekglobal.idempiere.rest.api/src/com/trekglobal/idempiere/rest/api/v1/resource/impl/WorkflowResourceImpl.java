b455efb63ff64b2a721d324046cee8446a61e146
Carlos Ruiz <carg67@gmail.com>
2022-06-20 17:19:00 +0200

    issues11 - implement reject, setuserchoice, forward, acknowledge

--- com.trekglobal.idempiere.rest.api/src/com/trekglobal/idempiere/rest/api/v1/resource/impl/WorkflowResourceImpl.java
+++ com.trekglobal.idempiere.rest.api/src/com/trekglobal/idempiere/rest/api/v1/resource/impl/WorkflowResourceImpl.java
@@ -31,11 +31,10 @@ import java.util.logging.Level;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import org.compiere.model.I_AD_WF_Activity;
+import org.compiere.model.MBPartner;
 import org.compiere.model.MUser;
 import org.compiere.model.Query;
 import org.compiere.util.CLogger;
-import org.compiere.util.DisplayType;
 import org.compiere.util.Env;
 import org.compiere.util.Msg;
 import org.compiere.util.Trx;
@@ -48,7 +47,6 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.trekglobal.idempiere.rest.api.json.RestUtils;
 import com.trekglobal.idempiere.rest.api.util.ErrorBuilder;
 import com.trekglobal.idempiere.rest.api.v1.resource.WorkflowResource;
@@ -71,7 +69,7 @@ public class WorkflowResourceImpl implements WorkflowResource {
 					.entity(new ErrorBuilder().status(Status.BAD_REQUEST).title("User not found").append("No user found matching id ").append(userId).build().toString())
 					.build();
 
-		List<MWFActivity> activities = getUserUnprocessedSuspendedActivities(AD_User_ID);
+		List<MWFActivity> activities = getUserPendingActivities(AD_User_ID);
 
 		JsonArray array = new JsonArray();
 		for (MWFActivity activity : activities) {
@@ -82,42 +80,25 @@ public class WorkflowResourceImpl implements WorkflowResource {
 		json.add("nodes", array);
 		return Response.ok(json.toString()).build();
 	}
-	
+
 	private int getAD_User_ID(String userId) {
-		if (Util.isEmpty(userId)) 
+		if (Util.isEmpty(userId))
 			return Env.getAD_User_ID(Env.getCtx());
-		
+
 		MUser user = (MUser) RestUtils.getPO(MUser.Table_Name, userId, true, false);
-		return user == null ? -1 : user.getAD_User_ID(); 
+		return user == null ? -1 : user.getAD_User_ID();
 	}
-	
-	private List<MWFActivity> getUserUnprocessedSuspendedActivities(int AD_User_ID) {
-		final String whereClause = 
-				"AD_WF_Activity.Processed=? AND AD_WF_Activity.WFState=? AND ("
-				//	Owner of Activity
-				+ " AD_WF_Activity.AD_User_ID=?"	//	#1
-				//	Invoker (if no invoker = all)
-				+ " OR EXISTS (SELECT * FROM AD_WF_Responsible r WHERE AD_WF_Activity.AD_WF_Responsible_ID=r.AD_WF_Responsible_ID"
-				+ " AND r.ResponsibleType='H' AND COALESCE(r.AD_User_ID,0)=0 AND COALESCE(r.AD_Role_ID,0)=0 AND (AD_WF_Activity.AD_User_ID=? OR AD_WF_Activity.AD_User_ID IS NULL))"	//	#2
-				//  Responsible User
-				+ " OR EXISTS (SELECT * FROM AD_WF_Responsible r WHERE AD_WF_Activity.AD_WF_Responsible_ID=r.AD_WF_Responsible_ID"
-				+ " AND r.ResponsibleType='H' AND r.AD_User_ID=?)"		//	#3
-				//	Responsible Role
-				+ " OR EXISTS (SELECT * FROM AD_WF_Responsible r INNER JOIN AD_User_Roles ur ON (r.AD_Role_ID=ur.AD_Role_ID)"
-				+ " WHERE AD_WF_Activity.AD_WF_Responsible_ID=r.AD_WF_Responsible_ID AND r.ResponsibleType='R' AND ur.AD_User_ID=? AND ur.isActive = 'Y')"	//	#4
-				///* Manual Responsible */ 
-				+ " OR EXISTS (SELECT * FROM AD_WF_ActivityApprover r "
-				+ " WHERE AD_WF_Activity.AD_WF_Activity_ID=r.AD_WF_Activity_ID AND r.AD_User_ID=? AND r.isActive = 'Y')" 
-				+ ")";
-		
-		return new Query(Env.getCtx(), I_AD_WF_Activity.Table_Name, whereClause, null)
-				.setParameters(false, MWFActivity.WFSTATE_Suspended, AD_User_ID, AD_User_ID, AD_User_ID, AD_User_ID, AD_User_ID)
+
+	private List<MWFActivity> getUserPendingActivities(int AD_User_ID) {
+		final String whereClause = getWhereUserPendingActivities(); // MWFActivity.getWhereUserPendingActivities();
+		return new Query(Env.getCtx(), MWFActivity.Table_Name, whereClause, null)
+				.setParameters(AD_User_ID, AD_User_ID, AD_User_ID, AD_User_ID, AD_User_ID, Env.getAD_Client_ID(Env.getCtx()))
 				.setClient_ID()
 				.setOnlyActiveRecords(true)
 				.setOrderBy("AD_WF_Activity.Priority DESC, AD_WF_Activity.Created")
 				.list();
 	}
-	
+
 	private JsonObject getActivityJsonObject(MWFActivity activity) {
 		JsonObject json = new JsonObject();
 		json.addProperty("id", activity.getAD_WF_Activity_ID());
@@ -145,36 +126,93 @@ public class WorkflowResourceImpl implements WorkflowResource {
 		int recordId = activity.getRecord_ID();
 		if (recordId > 0)
 			json.addProperty("record_id", recordId);
-		
+
 		return json;
 	}
-	
+
 	@Override
 	public Response approve(String nodeId, String jsonText) {
 		log.info("approve node: "+ nodeId);
 		log.info(jsonText);
-		int userId = Env.getAD_User_ID(Env.getCtx());
+		return actionActivity(nodeId, jsonText, "Y", true, false);
+	}
+
+	@Override
+	public Response reject(String nodeId, String jsonText) {
+		log.info("Reject node: "+ nodeId);
+		log.info(jsonText);
+		return actionActivity(nodeId, jsonText, "N", true, false);
+	}
+
+	@Override
+	public Response setUserChoice(String nodeId, String jsonText) {
+		log.info("setuserchoice node: "+ nodeId);
+		log.info(jsonText);
+		Gson gson = new GsonBuilder().create();
+		JsonObject jsonObject = gson.fromJson(jsonText, JsonObject.class);
+		String value = getValueProperty(jsonObject);
+		return actionActivity(nodeId, jsonText, value, false, false);
+	}
+
+	@Override
+	public Response acknowledge(String nodeId, String jsonText) {
+		log.info("acknowledge node: "+ nodeId);
+		log.info(jsonText);
+		return actionActivity(nodeId, jsonText, null, false, true);
+	}
+
+	/**
+	 * This method actions an Activity for approve, reject, setUserChoice or Acknowledge
+	 * @param nodeId - AD_WF_Activity_ID
+	 * @param jsonText - optional text containing the message
+	 * @param value - optional value for setUserChoice (for approve must be Y, for reject must be N)
+	 * @param isApproval - is this an approval node, true for approve and reject, false for the others
+	 * @param isConfirmation - is this a confirmation node, true for acknowledge
+	 * @return
+	 */
+	private Response actionActivity(String nodeId, String jsonText, String value, boolean isApproval, boolean isConfirmation) {
 		Trx trx = null;
 		MWFActivity activity = null;
 		try {
-			trx = Trx.get(Trx.createTrxName("RWFA"), true);
-			trx.setDisplayName(getClass().getName()+"_approve");
+			trx = Trx.get(Trx.createTrxName("RWFS"), true);
+			trx.setDisplayName(getClass().getName()+"_setUserChoice");
 			activity = (MWFActivity) RestUtils.getPO(MWFActivity.Table_Name, nodeId, true, true);
 			if (activity == null)
 				return Response.status(Status.NOT_FOUND)
 						.entity(new ErrorBuilder().status(Status.NOT_FOUND).title("Activity not found").build().toString())
 						.build();
-			activity.set_TrxName(trx.getTrxName());
-			if (!activity.getNode().isUserApproval())
-				return Response.status(Status.NOT_FOUND)
-						.entity(new ErrorBuilder().status(Status.NOT_FOUND).title("Not approval node").build().toString())
+			int currentUserId = Env.getAD_User_ID(Env.getCtx());
+			if (!isValidActionUser(currentUserId, activity))
+				return Response.status(Status.BAD_REQUEST)
+						.entity(new ErrorBuilder().status(Status.BAD_REQUEST)
+								.title("The current User cannot action this Activity")
+								.build().toString())
 						.build();
+
+			activity.set_TrxName(trx.getTrxName());
+			if (isApproval && !activity.getNode().isUserApproval())
+				return Response.status(Status.NOT_FOUND)
+						.entity(new ErrorBuilder().status(Status.NOT_FOUND).title("Not an approval node").build().toString())
+						.build();
+			if (isConfirmation && !activity.getNode().isUserManual())
+				return Response.status(Status.NOT_FOUND)
+						.entity(new ErrorBuilder().status(Status.NOT_FOUND).title("Not an acknowledgment node").build().toString())
+						.build();
+			if ( (!isApproval && !isConfirmation) && !activity.getNode().isUserChoice())
+				return Response.status(Status.NOT_FOUND)
+						.entity(new ErrorBuilder().status(Status.NOT_FOUND).title("Not a user choice node").build().toString())
+						.build();
+
 			String textMsg = null;
 			if (!Util.isEmpty(jsonText, true)) {
-				JsonParser parser = new JsonParser();
-				textMsg = getMessageProperty((JsonObject) parser.parse(jsonText));
+				Gson gson = new GsonBuilder().create();
+				JsonObject jsonObject = gson.fromJson(jsonText, JsonObject.class);
+				textMsg = getMessageProperty(jsonObject);
 			}
-			activity.setUserChoice(userId, "Y", DisplayType.YesNo, textMsg);
+			if (isConfirmation)
+				activity.setUserConfirmation(currentUserId, textMsg);
+			else
+				activity.setUserChoice(currentUserId, value, activity.getNode().getColumn().getAD_Reference_ID(), textMsg);
 			MWFProcess wfpr = new MWFProcess(activity.getCtx(), activity.getAD_WF_Process_ID(), activity.get_TrxName());
 			wfpr.checkCloseActivities(activity.get_TrxName());
 			trx.commit();
@@ -191,17 +229,13 @@ public class WorkflowResourceImpl implements WorkflowResource {
 		}
 		JsonObject json = new JsonObject();
 		json.addProperty("msg", Msg.getMsg(Env.getCtx(), "Updated"));
+		String summary = activity.getSummary();
+		if (!Util.isEmpty(summary, true))
+			json.addProperty("summary", summary);
 		String history = activity.getHistoryHTML();
 		if (!Util.isEmpty(history, true))
 			json.addProperty("history-records", history);
 		return Response.ok(json.toString()).build();
-	}
-	
-	@Override
-	public Response reject(String nodeId, String jsonText) {
-		log.info("Reject node: "+ nodeId);
-		log.info(jsonText);
-		return null;
 	}
 
 	@Override
@@ -212,49 +246,139 @@ public class WorkflowResourceImpl implements WorkflowResource {
 		if (user == null)
 			return Response.status(Status.BAD_REQUEST)
 					.entity(new ErrorBuilder().status(Status.BAD_REQUEST)
-					.title("userTo property is mandatory.")
-					.append("userTo property is not set or it has an invalid value").build().toString())
+							.title("userTo property is mandatory.")
+							.append("userTo property is not set or it has an invalid value").build().toString())
 					.build();
-		else if (user.getAD_User_ID() == Env.getAD_User_ID(Env.getCtx()))
+		// User validation: AD_Reference_ID=286 - AD_User - Internal - Employee or SalesRep
+		// EXISTS (SELECT * FROM C_BPartner bp
+		//    WHERE AD_User.C_BPartner_ID=bp.C_BPartner_ID AND (bp.IsEmployee='Y' OR bp.IsSalesRep='Y'))
+		MBPartner bp = null;
+		if (user.getC_BPartner_ID() > 0)
+			bp = MBPartner.get(Env.getCtx(), user.getC_BPartner_ID());
+		if (bp == null || ! ( bp.isEmployee() || bp.isSalesRep() ))
 			return Response.status(Status.BAD_REQUEST)
 					.entity(new ErrorBuilder().status(Status.BAD_REQUEST)
-					.title("userTo cannot be the same as the current user")
-					.build().toString())
+							.title("Invalid user - not Internal")
+							.build().toString())
 					.build();
-		
-		//TODO: logic to forward the node
-		
-		return null;
+
+		Trx trx = null;
+		MWFActivity activity = null;
+		try {
+			trx = Trx.get(Trx.createTrxName("RWFF"), true);
+			trx.setDisplayName(getClass().getName()+"_forward");
+			activity = (MWFActivity) RestUtils.getPO(MWFActivity.Table_Name, nodeId, true, true);
+			if (activity == null)
+				return Response.status(Status.NOT_FOUND)
+						.entity(new ErrorBuilder().status(Status.NOT_FOUND).title("Activity not found").build().toString())
+						.build();
+			int currentUserId = Env.getAD_User_ID(Env.getCtx());
+			if (!isValidActionUser(currentUserId, activity))
+				return Response.status(Status.BAD_REQUEST)
+						.entity(new ErrorBuilder().status(Status.BAD_REQUEST)
+								.title("The current User cannot action this Activity")
+								.build().toString())
+						.build();
+
+			activity.set_TrxName(trx.getTrxName());
+			String textMsg = null;
+			if (!Util.isEmpty(jsonText, true))
+				textMsg = getMessageProperty(jsonObject);
+			if (!activity.forwardTo(user.getAD_User_ID(), textMsg)) {
+				trx.rollback();
+				return Response.status(Status.NOT_MODIFIED)
+						.entity(new ErrorBuilder().status(Status.NOT_MODIFIED).title(Msg.getMsg(Env.getCtx(), "CannotForward")).build().toString())
+						.build();
+			}
+			trx.commit();
+		} catch (Exception ex) {
+			if (trx != null)
+				trx.rollback();
+			log.log(Level.SEVERE, ex.getMessage(), ex);
+			return Response.status(Status.INTERNAL_SERVER_ERROR)
+					.entity(new ErrorBuilder().status(Status.INTERNAL_SERVER_ERROR).title("Forward error").append("Forward error with exception: ").append(ex.getMessage()).build().toString())
+					.build();
+		} finally {
+			if (trx != null)
+				trx.close();
+		}
+		JsonObject json = new JsonObject();
+		json.addProperty("msg", Msg.getMsg(Env.getCtx(), "Updated"));
+		String summary = activity.getSummary();
+		if (!Util.isEmpty(summary, true))
+			json.addProperty("summary", summary);
+		String history = activity.getHistoryHTML();
+		if (!Util.isEmpty(history, true))
+			json.addProperty("history-records", history);
+		return Response.ok(json.toString()).build();
 	}
-	
+
 	private MUser getForwardToUser(JsonObject jsonObject) {
 		JsonElement jsonElement = jsonObject.get("userTo");
 		if (jsonElement == null || !jsonElement.isJsonPrimitive() || Util.isEmpty(jsonElement.getAsString(), true))
 			return null;
-		
+
 		String userId = jsonElement.getAsString();
 		return  (MUser) RestUtils.getPO(MUser.Table_Name, userId, true, false);
 	}
-	
-	private String getMessageProperty(JsonObject jsonObject) {
-		JsonElement jsonElement = jsonObject.get("message");
-		if (jsonElement == null || !jsonElement.isJsonPrimitive() || Util.isEmpty(jsonElement.getAsString(), true))
-			return "";
-		
-		return jsonElement.getAsString();		
-	}
-	
-	@Override
-	public Response setUserChoice(String nodeId, String jsonText) {
-		log.info("setuserchoice node: "+ nodeId);
-		log.info(jsonText);
-		return null;
+
+	private String getValueProperty(JsonObject jsonObject) {
+		return getStringProperty(jsonObject, "value");
 	}
 
-	@Override
-	public Response acknowledge(String nodeId, String jsonText) {
-		log.info("acknowledge node: "+ nodeId);
-		log.info(jsonText);
-		return null;
+	private String getMessageProperty(JsonObject jsonObject) {
+		return getStringProperty(jsonObject, "message");
 	}
+
+	private String getStringProperty(JsonObject jsonObject, String memberName) {
+		JsonElement jsonElement = jsonObject.get(memberName);
+		if (jsonElement == null || !jsonElement.isJsonPrimitive() || Util.isEmpty(jsonElement.getAsString(), true))
+			return "";
+
+		return jsonElement.getAsString();
+	}
+
+	/**
+	 * Validate if a user can action an Activity
+	 * @param AD_User_ID
+	 * @param activity
+	 * @return
+	 */
+	private boolean isValidActionUser(int AD_User_ID, MWFActivity activity) {
+		String whereClause = getWhereUserPendingActivities(); // MWFActivity.getWhereUserPendingActivities();
+		whereClause += " AND AD_WF_Activity.AD_WF_Activity_ID=?";
+		int cnt = new Query(Env.getCtx(), MWFActivity.Table_Name, whereClause, null)
+				.setParameters(AD_User_ID, AD_User_ID, AD_User_ID, AD_User_ID, AD_User_ID, Env.getAD_Client_ID(Env.getCtx()), activity.getAD_WF_Activity_ID())
+				.setClient_ID()
+				.setOnlyActiveRecords(true)
+				.count();
+		return (cnt == 1);
+	}
+
+	/**
+	 * Where to get the pending activities related to a User (unprocessed and suspended)
+	 * The where returned requires the AD_User_ID parameter 5 times, and then AD_Client_ID
+	 * @return Where Clause
+	 */
+	private static String getWhereUserPendingActivities() {
+		final String where =
+			"AD_WF_Activity.Processed='N' AND AD_WF_Activity.WFState='OS' AND ("
+			//	Owner of Activity
+			+ " AD_WF_Activity.AD_User_ID=?"	//	#1
+			//	Invoker (if no invoker = all)
+			+ " OR EXISTS (SELECT * FROM AD_WF_Responsible r WHERE AD_WF_Activity.AD_WF_Responsible_ID=r.AD_WF_Responsible_ID"
+			+ " AND r.ResponsibleType='H' AND COALESCE(r.AD_User_ID,0)=0 AND COALESCE(r.AD_Role_ID,0)=0 AND (AD_WF_Activity.AD_User_ID=? OR AD_WF_Activity.AD_User_ID IS NULL))"	//	#2
+			//  Responsible User
+			+ " OR EXISTS (SELECT * FROM AD_WF_Responsible r WHERE AD_WF_Activity.AD_WF_Responsible_ID=r.AD_WF_Responsible_ID"
+			+ " AND r.ResponsibleType='H' AND r.AD_User_ID=?)"		//	#3
+			//	Responsible Role
+			+ " OR EXISTS (SELECT * FROM AD_WF_Responsible r INNER JOIN AD_User_Roles ur ON (r.AD_Role_ID=ur.AD_Role_ID)"
+			+ " WHERE AD_WF_Activity.AD_WF_Responsible_ID=r.AD_WF_Responsible_ID AND r.ResponsibleType='R' AND ur.AD_User_ID=? AND ur.isActive = 'Y')"	//	#4
+			///* Manual Responsible */
+			+ " OR EXISTS (SELECT * FROM AD_WF_ActivityApprover r "
+			+ " WHERE AD_WF_Activity.AD_WF_Activity_ID=r.AD_WF_Activity_ID AND r.AD_User_ID=? AND r.isActive = 'Y')"
+			+ ") AND AD_WF_Activity.AD_Client_ID=?";	//	#5
+		return where;
+	}
+
 }
