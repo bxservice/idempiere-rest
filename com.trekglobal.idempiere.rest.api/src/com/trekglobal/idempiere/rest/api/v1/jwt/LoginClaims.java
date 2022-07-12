d406dc0e4612f808122b2ad88d8a5498fb66a8a5
Carlos Ruiz <carg67@gmail.com>
2022-07-12 12:52:47 +0200

    Issue #104 - REST allows login with wrong roleId (#105)
    * Issue #104 - REST allows login with wrong roleId
    
    * Document one step login in postman

--- com.trekglobal.idempiere.rest.api/src/com/trekglobal/idempiere/rest/api/v1/jwt/LoginClaims.java
+++ com.trekglobal.idempiere.rest.api/src/com/trekglobal/idempiere/rest/api/v1/jwt/LoginClaims.java
@@ -31,5 +31,5 @@ package com.trekglobal.idempiere.rest.api.v1.jwt;
  *
  */
 public enum LoginClaims {
-	AD_Client_ID, AD_User_ID, AD_Role_ID, AD_Org_ID, M_Warehouse_ID, AD_Language, AD_Session_ID
+	AD_Client_ID, AD_User_ID, AD_Role_ID, AD_Org_ID, M_Warehouse_ID, AD_Language, AD_Session_ID, Clients
 }
