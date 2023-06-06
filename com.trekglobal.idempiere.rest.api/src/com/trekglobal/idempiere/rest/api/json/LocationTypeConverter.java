96c9fddc06dbab99604340765ab6bc1016c0be6d
muriloht <muriloht@devcoffee.com.br>
2023-06-05 23:11:50 -0300

    issue #162 New Location Type Converter

---
+++ com.trekglobal.idempiere.rest.api/src/com/trekglobal/idempiere/rest/api/json/LocationTypeConverter.java
@@ -0,0 +1,349 @@
+/**********************************************************************
+* This file is part of iDempiere ERP Open Source                      *
+* http://www.idempiere.org                                            *
+*                                                                     *
+* Copyright (C) Contributors                                          *
+*                                                                     *
+* This program is free software; you can redistribute it and/or       *
+* modify it under the terms of the GNU General Public License         *
+* as published by the Free Software Foundation; either version 2      *
+* of the License, or (at your option) any later version.              *
+*                                                                     *
+* This program is distributed in the hope that it will be useful,     *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        *
+* GNU General Public License for more details.                        *
+*                                                                     *
+* You should have received a copy of the GNU General Public License   *
+* along with this program; if not, write to the Free Software         *
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,          *
+* MA 02110-1301, USA.                                                 *
+*                                                                     *
+* Contributors:                                                       *
+* - Trek Global Corporation                                           *
+* - Heng Sin Low                                                      *
+**********************************************************************/
+package com.trekglobal.idempiere.rest.api.json;
+
+import org.compiere.model.GridField;
+import org.compiere.model.Lookup;
+import org.compiere.model.MCity;
+import org.compiere.model.MColumn;
+import org.compiere.model.MCountry;
+import org.compiere.model.MLocation;
+import org.compiere.model.MLocationLookup;
+import org.compiere.model.MProcessPara;
+import org.compiere.model.MRegion;
+import org.compiere.util.Env;
+import org.compiere.util.Msg;
+import org.compiere.util.Util;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+
+/**
+ * json type converter for C_Location
+ * @author muriloht (devCoffee, muriloht@devcoffee.com.br)
+ *
+ */
+public class LocationTypeConverter implements ITypeConverter<Object> {
+
+	/**
+	 * default constructor
+	 */
+	public LocationTypeConverter() {
+		
+	}
+
+	@Override
+	public Object toJsonValue(MColumn column, Object value) {
+		String label = Msg.getElement(Env.getCtx(), column.getColumnName());
+		Lookup lookup = new MLocationLookup(Env.getCtx(), 0);
+		return toJsonValue(column.getAD_Reference_ID(), label, lookup, column.getReferenceTableName(), value);
+	}
+
+	@Override
+	public Object toJsonValue(GridField field, Object value) {
+		return toJsonValue(field.getDisplayType(), field.getHeader(), field.getLookup(), getReferenceTableNameFromField(field), value);
+	}
+	
+	private String getReferenceTableNameFromField(GridField field) {
+		String refTableName = null;
+		if (field.getVO().isProcess) {
+			MProcessPara pp = field.getAD_Column_ID() > 0 ? MProcessPara.get(field.getAD_Column_ID()) : null;
+			if (pp != null)
+				refTableName = pp.getReferenceTableName();
+		} else {
+			MColumn column = field.getAD_Column_ID() > 0 ? MColumn.get(Env.getCtx(), field.getAD_Column_ID()) : null;
+			if (column != null)
+				refTableName = column.getReferenceTableName();
+		}
+		return refTableName;
+	}
+	
+	private Object toJsonValue(int displayType, String label, Lookup lookup, String refTableName, Object value) {
+		if (lookup != null) {
+			
+			MLocation loc = MLocation.get((Integer)value);
+			
+			JsonObject ref = new JsonObject();
+			ref.addProperty("propertyLabel", label);
+			if (value instanceof Number)
+				ref.addProperty("id", ((Number)value).intValue());
+			else
+				ref.addProperty("id", value.toString());
+			String display = lookup.getDisplay(value);
+			if (!Util.isEmpty(display, true)) {
+				ref.addProperty("identifier", display);
+			}							
+			if (!Util.isEmpty(refTableName))
+				ref.addProperty("model-name", refTableName.toLowerCase());
+			
+			if (!Util.isEmpty(loc.getAddress1()))
+				ref.addProperty(MLocation.COLUMNNAME_Address1, loc.getAddress1());
+			if (!Util.isEmpty(loc.getAddress2()))
+				ref.addProperty(MLocation.COLUMNNAME_Address2, loc.getAddress2());
+			if (!Util.isEmpty(loc.getAddress3()))
+				ref.addProperty(MLocation.COLUMNNAME_Address3, loc.getAddress3());
+			if (!Util.isEmpty(loc.getAddress4()))
+				ref.addProperty(MLocation.COLUMNNAME_Address4, loc.getAddress4());
+			if (!Util.isEmpty(loc.getAddress5()))
+				ref.addProperty(MLocation.COLUMNNAME_Address5, loc.getAddress5());
+			if (!Util.isEmpty(loc.getPostal()))
+				ref.addProperty(MLocation.COLUMNNAME_Postal, loc.getAddress5());
+			if (!Util.isEmpty(loc.getPostal_Add()))
+				ref.addProperty(MLocation.COLUMNNAME_Postal_Add, loc.getPostal_Add());
+			
+			if (loc.getC_Country_ID() > 0) {
+				MCountry c = MCountry.get(loc.getC_Country_ID());
+				JsonObject refChild = new JsonObject();
+				refChild.addProperty("propertyLabel", Msg.getElement(Env.getCtx(), MCountry.COLUMNNAME_C_Country_ID));
+				if (value instanceof Number)
+					refChild.addProperty("id", c.get_ID());
+				else
+					refChild.addProperty("id", value.toString());
+				if (!Util.isEmpty(c.getCountryCode(), true)) {
+					refChild.addProperty("identifier", c.getCountryCode());
+				}							
+				refChild.addProperty("model-name", MCountry.Table_Name.toLowerCase());
+				ref.add(MCountry.COLUMNNAME_C_Country_ID, refChild);
+
+			}
+			
+			if (loc.getC_Region_ID() > 0) {
+				MRegion r = MRegion.get(loc.getC_Region_ID());
+				JsonObject refChild = new JsonObject();
+				refChild.addProperty("propertyLabel", Msg.getElement(Env.getCtx(), MRegion.COLUMNNAME_C_Region_ID));
+				if (value instanceof Number)
+					refChild.addProperty("id", r.get_ID());
+				else
+					refChild.addProperty("id", value.toString());
+				if (!Util.isEmpty(r.getName(), true)) {
+					refChild.addProperty("identifier", r.getName());
+				}							
+				refChild.addProperty("model-name", MRegion.Table_Name.toLowerCase());
+				ref.add(MRegion.COLUMNNAME_C_Region_ID, refChild);
+			}
+			
+			if (!Util.isEmpty(loc.getRegionName()))
+				ref.addProperty(MLocation.COLUMNNAME_RegionName, loc.getRegionName());
+			
+			if (loc.getC_City_ID() > 0) {
+				MCity c = MCity.get(loc.getC_City_ID());
+				JsonObject refChild = new JsonObject();
+				refChild.addProperty("propertyLabel", Msg.getElement(Env.getCtx(), MCity.COLUMNNAME_C_City_ID));
+				if (value instanceof Number)
+					refChild.addProperty("id", c.get_ID());
+				else
+					refChild.addProperty("id", value.toString());
+				if (!Util.isEmpty(c.getName(), true)) {
+					refChild.addProperty("identifier", c.getName());
+				}							
+				refChild.addProperty("model-name", MRegion.Table_Name.toLowerCase());
+				ref.add(MCity.COLUMNNAME_C_City_ID, refChild);
+			}
+			
+			if (!Util.isEmpty(loc.getCity()))
+				ref.addProperty(MLocation.COLUMNNAME_City, loc.getCity());
+			
+			if (!Util.isEmpty(loc.getComments()))
+				ref.addProperty(MLocation.COLUMNNAME_Comments, loc.getComments());
+			
+			ref.addProperty(MLocation.COLUMNNAME_IsActive, loc.isActive());
+			
+			return ref;
+		} else {
+			return null;
+		}
+	}
+		
+	@Override
+	public Object fromJsonValue(GridField field, JsonElement value) {
+		return fromJson(value);
+	}
+
+	@Override
+	public Object fromJsonValue(MColumn column, JsonElement value) {
+		return fromJson(value);
+	}
+	
+	public Object fromJson(JsonElement value) {
+		if (value != null && value.isJsonObject()) {
+		
+			JsonObject ref = value.getAsJsonObject();
+			JsonElement idField = ref.get("id");
+			int C_Location_ID = 0;
+			if (idField != null) {
+				JsonPrimitive primitive = (JsonPrimitive) idField;
+				if (primitive.isNumber())
+					C_Location_ID = primitive.getAsInt();
+				else
+					C_Location_ID = 0;
+			}
+
+			JsonElement address1Field = ref.get(MLocation.COLUMNNAME_Address1.toLowerCase());
+			String address1 = null;
+			if (address1Field != null) {
+				JsonPrimitive primitive = (JsonPrimitive) address1Field;
+				address1 = primitive.getAsString();
+			}
+			
+			JsonElement address2Field = ref.get(MLocation.COLUMNNAME_Address2.toLowerCase());
+			String address2 = null;
+			if (address2Field != null) {
+				JsonPrimitive primitive = (JsonPrimitive) address2Field;
+				address2 = primitive.getAsString();
+			}
+			
+			JsonElement address3Field = ref.get(MLocation.COLUMNNAME_Address3.toLowerCase());
+			String address3 = null;
+			if (address3Field != null) {
+				JsonPrimitive primitive = (JsonPrimitive) address3Field;
+				address3 = primitive.getAsString();
+			}
+			
+			JsonElement address4Field = ref.get(MLocation.COLUMNNAME_Address3.toLowerCase());
+			String address4 = null;
+			if (address3Field != null) {
+				JsonPrimitive primitive = (JsonPrimitive) address4Field;
+				address4 = primitive.getAsString();
+			}
+			
+			JsonElement address5Field = ref.get(MLocation.COLUMNNAME_Address5.toLowerCase());
+			String address5 = null;
+			if (address5Field != null) {
+				JsonPrimitive primitive = (JsonPrimitive) address5Field;
+				address5 = primitive.getAsString();
+			}
+			
+			JsonElement postalField = ref.get(MLocation.COLUMNNAME_Postal.toLowerCase());
+			String postal = null;
+			if (postalField != null) {
+				JsonPrimitive primitive = (JsonPrimitive) postalField;
+				postal = primitive.getAsString();
+			}
+			
+			JsonElement postalAddField = ref.get(MLocation.COLUMNNAME_Postal_Add.toLowerCase());
+			String postalAdd = null;
+			if (postalAddField != null) {
+				JsonPrimitive primitive = (JsonPrimitive) postalAddField;
+				postalAdd = primitive.getAsString();
+			}
+			
+			JsonElement countryField = ref.get(MLocation.COLUMNNAME_C_Country_ID.toLowerCase());
+			int C_Country_ID = 0;
+			if (countryField != null) {
+				JsonPrimitive primitive = (JsonPrimitive) countryField;
+				if (primitive.isNumber())
+					C_Country_ID = primitive.getAsInt();
+			}
+			
+			JsonElement regionField = ref.get(MLocation.COLUMNNAME_C_Region_ID.toLowerCase());
+			int C_Region_ID = 0;
+			if (countryField != null) {
+				JsonPrimitive primitive = (JsonPrimitive) regionField;
+				if (primitive.isNumber())
+					C_Region_ID = primitive.getAsInt();
+			}
+			
+			JsonElement regionNameField = ref.get(MLocation.COLUMNNAME_RegionName.toLowerCase());
+			String regionName = null;
+			if (regionNameField != null) {
+				JsonPrimitive primitive = (JsonPrimitive) regionNameField;
+				regionName = primitive.getAsString();
+			}
+			
+			JsonElement cityIDField = ref.get(MLocation.COLUMNNAME_C_City_ID.toLowerCase());
+			int C_City_ID = 0;
+			if (cityIDField != null) {
+				JsonPrimitive primitive = (JsonPrimitive) cityIDField;
+				if (primitive.isNumber())
+					C_City_ID = primitive.getAsInt();
+			}
+			
+			JsonElement cityField = ref.get(MLocation.COLUMNNAME_City.toLowerCase());
+			String city = null;
+			if (cityField != null) {
+				JsonPrimitive primitive = (JsonPrimitive) cityField;
+				city = primitive.getAsString();
+			}
+			
+			JsonElement commentsField = ref.get(MLocation.COLUMNNAME_Comments.toLowerCase());
+			String comments = null;
+			if (commentsField != null) {
+				JsonPrimitive primitive = (JsonPrimitive) commentsField;
+				comments = primitive.getAsString();
+			}
+			
+			JsonElement isActiveField = ref.get(MLocation.COLUMNNAME_IsActive.toLowerCase());
+			boolean isActive = false;
+			if (isActiveField != null) {
+				JsonPrimitive primitive = (JsonPrimitive) isActiveField;
+				isActive = primitive.getAsBoolean();
+			}
+			
+			MLocation loc = new MLocation(Env.getCtx(), C_Location_ID, null);
+			
+			if (address1 != null)
+				loc.setAddress1(address1);
+			if (address2 != null)
+				loc.setAddress2(address2);
+			if (address3 != null)
+				loc.setAddress3(address3);
+			if (address4 != null)
+				loc.setAddress4(address4);
+			if (address5 != null)
+				loc.setAddress5(address5);
+			if (postal != null)
+				loc.setPostal(postal);
+			if (postalAdd != null)
+				loc.setPostal_Add(postalAdd);
+			
+			if (C_Country_ID > 0)
+				loc.setC_Country_ID(C_Country_ID);
+			
+			if (C_Region_ID > 0)
+				loc.setC_Region_ID(C_Region_ID);
+			
+			if (regionName != null)
+				loc.setRegionName(regionName);
+			
+			if (C_City_ID > 0)
+				loc.setC_City_ID(C_City_ID);
+			
+			if (city != null)
+				loc.setCity(city);
+			
+			if (comments != null)
+				loc.setComments(comments);
+			
+			loc.setIsActive(isActive);
+			loc.saveEx();
+			
+			return loc.get_ID();
+		} 
+		
+		return null;
+	}
+}
--- com.trekglobal.idempiere.rest.api/src/com/trekglobal/idempiere/rest/api/json/TypeConverterUtils.java
+++ com.trekglobal.idempiere.rest.api/src/com/trekglobal/idempiere/rest/api/json/TypeConverterUtils.java
@@ -185,8 +185,9 @@ public class TypeConverterUtils {
 				typeConverter = new DateTypeConverter();
 			} else if (DisplayType.YesNo == displayType) {
 				typeConverter = new YesNoTypeConverter();
-			} else if (displayType == Location
-					|| displayType == Locator
+			} else if (displayType == Location) {
+					return new LocationTypeConverter();
+			} else if (displayType == Locator
 					|| displayType == Account
 					|| displayType == PAttribute
 					|| displayType == Payment
@@ -214,8 +215,9 @@ public class TypeConverterUtils {
 				typeConverter = new DateTypeConverter();
 			} else if (DisplayType.YesNo == displayType && (isBoolean(value) || isString(value))) {
 				typeConverter = new YesNoTypeConverter();
-			} else if (displayType == Location
-					|| displayType == Locator
+			} else if (displayType == Location) {
+				return new LocationTypeConverter();
+			} else if (displayType == Locator
 					|| displayType == Account
 					|| displayType == PAttribute
 					|| displayType == Payment
