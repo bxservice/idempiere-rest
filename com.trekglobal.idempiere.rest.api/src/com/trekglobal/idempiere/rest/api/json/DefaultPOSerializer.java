5f75e14dc51bc3b4a1b3563fae1c88ae6de86186
Diego Ruiz <12065321+d-ruiz@users.noreply.github.com>
2024-01-17 19:10:54 +0100

    Issue 202 (#204)
    * issue-202 Refactor code
    
    * issue-202 Check column access on update/create

--- com.trekglobal.idempiere.rest.api/src/com/trekglobal/idempiere/rest/api/json/DefaultPOSerializer.java
+++ com.trekglobal.idempiere.rest.api/src/com/trekglobal/idempiere/rest/api/json/DefaultPOSerializer.java
@@ -93,8 +93,7 @@ public class DefaultPOSerializer implements IPOSerializer, IPOSerializerFactory 
 			MColumn column = MColumn.get(Env.getCtx(), poInfo.getAD_Column_ID(columnName));
 			if (column.isSecure() || column.isEncrypted())
 				continue;
-			// Check if the role has access to this column
-			if (!MRole.getDefault(po.getCtx(), false).isColumnAccess(po.get_Table_ID(), column.getAD_Column_ID(), true))
+			if (!hasRoleColumnAccess(po.get_Table_ID(), column.getAD_Column_ID(), true))
 				continue;
 
 			Object value ;
@@ -252,6 +251,14 @@ public class DefaultPOSerializer implements IPOSerializer, IPOSerializerFactory 
 					return false;
 			}
 		}
+		
+		if (!hasRoleColumnAccess(column.getAD_Table_ID(), column.getAD_Column_ID(), false)) {
+			if (errorOnNonUpdatable)
+				throw new AdempiereException("Cannot update column " + column.getColumnName());
+			else
+				return false;
+		}
+			
 		return true;
 	}
 
@@ -310,6 +317,18 @@ public class DefaultPOSerializer implements IPOSerializer, IPOSerializerFactory 
 				return true;
 		}
 		return false;
+	}
+	
+	/**
+	 * Check if the role has access to this column
+	 * @param AD_Table_ID
+	 * @param AD_Column_ID
+	 * @param readOnly
+	 * @return true if user has access
+	 */
+	private boolean hasRoleColumnAccess(int AD_Table_ID, int AD_Column_ID, boolean readOnly) {
+		return MRole.getDefault(Env.getCtx(), false).isColumnAccess(AD_Table_ID, AD_Column_ID, readOnly);
+
 	}
 	
 	private void setDefaultValue(PO po, MColumn column) {
